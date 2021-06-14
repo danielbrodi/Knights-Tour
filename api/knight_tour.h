68b83a2f6c60e5792db8a89b0121b0caf0e34594
danielbrodi <broddanik@gmail.com>
2021-06-14 17:12:43 +0300

    pseudocode

---
+++ api/knight_tour.h
@@ -0,0 +1,13 @@
+/*********************************FILE__HEADER*********************************\
+* File:				knight_tour.h						 		  								
+* Author:			Daniel Brodsky					  								
+* Date:				14-06-2021							   												   								
+* Description:		knight_tour implementation API.		 
+\******************************************************************************/
+
+#ifndef __knight_tour_H__
+#define __knight_tour_H__
+
+#include <stddef.h>		/*	size_t	*/
+
+#endif	/* __knight_tour_H__	*/
---
+++ src/knight_tour.c
@@ -0,0 +1,47 @@
+/*********************************FILE__HEADER*********************************\
+* File:				knight_tour.c
+* Author:			Daniel Brodsky				 		  												  								
+* Date:				14-06-2021
+* Code Reviewer:	
+* Pseudo Reviewer: 						   								
+* Version:			1.0   								
+* Description:		Find out if it's possible for a knight at a given position
+					to fully go through a chess board without stepping at a
+					location more than one time.
+\******************************************************************************/
+
+/********************************* Inclusions *********************************/
+
+
+/******************************************************************************/
+int Tour(int position, unsigned char path[64])
+{
+/*	asserts*/
+/*	*/
+/*	unsigned long board = 0;*/
+/*	*/
+/*	return (TourImp(path, position, board, 1));*/
+}
+/******************************************************************************/
+int TourImp(unsigned char path[64], int pos, unsigned long board, int step_num)
+{
+/*	asserts*/
+/*	*/
+/*	if this is the last step*/
+/*		update path to be as boar*/
+/*		return 0*/
+/*		*/
+/*	if this is out of bounds OR*/
+/*		return 1*/
+/*		*/
+/*	if this is already a visited location*/
+/*		return 1*/
+/*		*/
+/*	(may combine the 2 failure as one if with OR opeartor)*/
+/*	*/
+/*	recursively call 8 available positions with AND opeartor between them*/
+/*	*/
+/*	if the result of this AND is zero it means some one them has succeeed so return 0*/
+/*	otherwise return 1.*/
+	
+}
